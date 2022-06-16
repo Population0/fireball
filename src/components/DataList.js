313580b9521fec0c4b4c16240704e7a78da4b322
Jha, Pragya <pragya.jha@troweprice.com>
2022-06-16 14:51:26 -0400

    CircleChart

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
 // import Fireball from './Fireball'
 import BarChart from "./BarChart";
+import CircleDiagram from "./CircleDiagram";
 
 
 
 const DataList = (props) =>{
   let velocity_data=props.data.map((fireball)=>{return{x:fireball.date, y:fireball.velocity}});
   let energy_data=props.data.map((fireball)=>{return{x:fireball.date, y:fireball.energy}});
-  const [graphState, setGraphState]=useState(true);
+  const [graphState, setGraphState]=useState(0);
   let renderCounter=0;
 console.log(renderCounter)
   const graphStateHandler=()=>{
-    if(graphState){
+    if(graphState===1){
     return {title:"energy",data:energy_data.sort()};
-    }else{
+    }else if(graphState===0){
   return {title:"velocity",data:velocity_data.sort()};
+    }else{
+      return {title:"ETL Processes",data:[1,2,0]};
     }
   }
     return (
       <React.Fragment>
-        <button onClick={()=>setGraphState(false)}>Velocity</button> 
-        <button onClick={()=>setGraphState(true)}>Energy</button> 
-          <BarChart dataProp={graphStateHandler()}/>
+        <button onClick={()=>setGraphState(0)}>Velocity</button> 
+        <button onClick={()=>setGraphState(1)}>Energy</button> 
+        <button onClick={()=>setGraphState(2)}>Circle Diagram</button> 
+          {graphState!==2?<BarChart dataProp={graphStateHandler()}/>:<CircleDiagram dataProp={graphStateHandler()}/>}
 
         </React.Fragment>
       );
